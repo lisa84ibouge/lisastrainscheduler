ee71c452985a56ef86f6d6489100f915a1c1a880
Lisa Leslie <lisar@ibouge.com>
2019-02-05 22:03:05 -0800

    working form field inputs

--- trainscheduler.js
+++ trainscheduler.js
@@ -1,28 +1,31 @@
 
-//   var config = {
-//     apiKey: "AIzaSyDtAYLv0z7YNeeBz10AkR0TaZ9ixQZfQYU",
-//     authDomain: "trainschedulerapp-3d9f9.firebaseapp.com",
-//     databaseURL: "https://trainschedulerapp-3d9f9.firebaseio.com",
-//     projectId: "trainschedulerapp-3d9f9",
-//     storageBucket: "trainschedulerapp-3d9f9.appspot.com",
-//     messagingSenderId: "1015683100147"
-//   };
-//   firebase.initializeApp(config);
-
-//  var database = firebase.database();
-
-$(document).ready(function () {
+  $(document).ready(function () {
     //your code here
   
-  
-
+    
   $('#submitButton').on("click", function (event){
         event.preventDefault();
         console.log("click")
-        var trainName = $('#trainName').val.trim();
-        var destination = $('#destination').val.trim();
-        var firstTrain = $('#firstTrain').val.trim();
-        var frequency = $('#frequency').val.trim();
+        var trainName = $('#trainName').val().trim();
+        var destination = $('#destination').val().trim();
+        var firstTrain = $('#firstTrain').val().trim();
+        var frequency = $('#frequency').val().trim();
         console.log(trainName, destination, firstTrain, frequency);
+
+        var tr = $('<tr>');
+        var td1 = $('<td>').html(trainName);
+        var td2 = $('<td>').html(destination);
+        var td3 = $('<td>').html(firstTrain);
+        var td4 = $('<td>').html(frequency);
+
+        tr.append(td1, td2, td3, td4);
+        $('#trainBody').append(tr);
   })
+ 
+
+
+
+
+
+
 });
