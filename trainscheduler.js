c56cd5754e7bc7c0ddfde420cfd9130cce859ecd
Lisa Leslie <lisar@ibouge.com>
2019-02-07 22:54:40 -0800

    working read functionality for database

--- trainscheduler.js
+++ trainscheduler.js
@@ -1,9 +1,36 @@
   $(document).ready(function () {
     //your code here
     
-    var trainCount = firebase.database().ref('trains/'+ trainCount + '/trainCount')
+    var trainCount = firebase.database().ref('trains/');
     trainCount.on('value', function(snapshot) {
-    updateTrainCount(trainName, snapshot.val());
+      var val = snapshot.val();
+      for (var key in val){
+        var trainName = val[key].trainName;
+        var destination = val[key].destination;
+        var frequency = val[key].frequency;
+        var firstTrain = val[key].firstTrain;
+        var nextTrain = moment(firstTrain);
+        while (nextTrain.isBefore(moment())) {
+            nextTrain = nextTrain.add(parseInt(frequency), 'minutes');
+          
+        }
+        console.log(nextTrain);
+
+
+        var timeRemaining = moment().diff(nextTrain);
+
+        var tr = $('<tr>');
+        var td1 = $('<td>').html(trainName);
+        var td2 = $('<td>').html(destination);
+        var td3 = $('<td>').html(frequency);
+        var td4 = $('<td>').html(nextTrain.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+        var td5 = $('<td>').html(nextTrain.fromNow());
+
+        tr.append(td1, td2, td3, td4, td5);
+        $('#trainBody').append(tr);
+
+      }
+   // trainCount(trainName, snapshot.val());
     console.log(snapshot.val());
 
 });
@@ -46,6 +73,8 @@
 
         tr.append(td1, td2, td3, td4, td5);
         $('#trainBody').append(tr);
+
+      
   })
  
 
